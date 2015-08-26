ae161c8f00fc6a021551d0c178fe37d5240aa6ca
Sachidananda Panigrahi <snpanigrahi88@gmail.com>
2015-08-26 15:58:13 +0530

    working with node

---
+++ server.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Sachidananda.Panigra on 8/26/2015.
+ */
+var express = require('express'),
+//    api     = require('./api'),
+//    users   = require('./accounts'),
+    app     = express();
+
+app
+    .use(express.static('./public'))
+//    .use(users)
+//    .use('/api', api)
+    .get('*', function (req, res) {
+        /*if (!req.user) {
+            res.redirect('/login');
+        } else {
+            res.sendFile('public/main.html', {"root": "."});
+        }*/
+        res.sendFile('public/main.html', {"root": "."});
+    })
+    .listen(4000);
+console.log("Server is listing on the port 4000");
