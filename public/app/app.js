8279584807832264cc44a33b378f55ff950ac787
pbhoi144 <pbhoi144@gmail.com>
2015-08-28 17:28:09 +0530

    update view page

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,4 @@
-/**
- * Created by Pramod on 27-08-2015.
- */
+/** Created by Pramod on 27-08-2015. **/
 angular.module('ContactsApp',[])
 .run(function($rootScope){
         $rootScope.message = "hi";
