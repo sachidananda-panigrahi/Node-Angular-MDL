b797cdb1700968db967535a6ac1b94c00128beb1
Sachidananda Panigrahi <snpanigrahi88@gmail.com>
2015-08-28 18:55:46 +0530

    working on template using ng-route

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,22 @@
 /**
  * Created by Sachidananda on 27-08-2015.
  */
-angular.module('ContactsApp', [])
-.run(function($rootScope){
-        $rootScope.message = "Hi";
+angular.module('contactsApp', ['ngRoute'])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/contacts', {
+                controller: 'ListController',
+                templateUrl: 'views/all-contacts.html'
+            })
+            .otherwise({
+                redirectTo: '/contacts'
+            });
+        $locationProvider.html5Mode(true);
     })
+    .run(function ($rootScope,$timeout) {
+        $rootScope.$on('$viewContentLoaded', function() {
+            $timeout(function() {
+                componentHandler.upgradeAllRegistered();
+    })
+})
+});
--- public/app/controller/controllers.js
+++ public/app/controller/controllers.js
@@ -1,3 +1,7 @@
 /**
  * Created by Sachidananda on 26-08-2015.
  */
+var app = angular.module('contactsApp');
+app.controller('ListController', function(){
+
+})
